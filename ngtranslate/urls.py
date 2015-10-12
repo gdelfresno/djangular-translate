2d5d0198262368fa123fc085ff2e3e66ccdce2c1
Anton A. Lysenkov <al@nerdpeople.com>
2015-10-12 17:16:56 +0300

    csrf extept change lang

--- ngtranslate/urls.py
+++ ngtranslate/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import LanguagesView, djangular_catalog
 
 urlpatterns = [
     url(r'catalog/', djangular_catalog, name='catalog'),
-    url(r'languages/', LanguagesView.as_view(), name='languages')
+    url(r'languages/', csrf_exempt(LanguagesView.as_view()), name='languages')
 ]
