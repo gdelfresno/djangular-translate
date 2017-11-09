c88fc761c1284c12642f8dd24dab42a76c9b729e
Guillermo del Fresno <gdelfresno@gmail.com>
2017-11-09 10:43:30 -0300

    Change make messages.

--- ngtranslate/management/commands/makemessages.py
+++ ngtranslate/management/commands/makemessages.py
@@ -1,81 +1,108 @@
 # -*- coding: utf-8 -*-
-import os
 import io
+import os
 import re
-from functools import total_ordering
-from itertools import repeat
-
 from django.conf import settings
-from django.utils.six import StringIO
+from django.core.files.temp import NamedTemporaryFile
 from django.core.management.base import CommandError
 from django.core.management.commands import makemessages
-from django.utils.translation.trans_real import blankout
+from django.core.management.commands.makemessages import (write_pot_file,
+                                                          STATUS_OK)
+from django.core.management.utils import popen_wrapper
+from django.utils.functional import cached_property
 
 NO_LOCALE_DIR = object()
 
 
-def djangularize(src, origin=None):
-    out = StringIO('')
-    offset = 0
-    directive_re = re.compile(r'<(?P<tag_name>\w+)[-=\/\"\'\s\w]*'
-                              r'(dj-translatable)[-=\/\"\'\s\w]*\/?>',
-                              re.IGNORECASE | re.DOTALL | re.VERBOSE)
-    for match in directive_re.finditer(src):
-        msg = None
-        msg_out = None
-        msg_length = 0
-        tag_str = repr(match.group())
-        out.write(blankout(src[offset:match.start()], 'X'))
-        offset = match.start()
-        is_closed_tag = ((re.search(r'\/>', tag_str)) is not None)
-        trans_attribute = re.search(r'dj-translatable\s*=\s*\"'
-                                    r'(?P<attr_name>[-\w\s]+)\"', tag_str)
-        if trans_attribute:
-            trans_attr_name = trans_attribute.group('attr_name').strip()
-            attr_val_re = r''.join([trans_attr_name,
-                                    r'\s*=\s*\"(?P<attr_value>[\w\s]+)\"'])
-            attr_value = re.search(attr_val_re, tag_str)
-            if attr_value:
-                msg = attr_value.group('attr_value').strip()
-                msg_out = ' gettext(%r) ' % msg
-                msg_length = len(msg_out)
-                out.write(msg_out)
-        if not is_closed_tag:
-            tag_name = match.group('tag_name')
-            tag_closer_re = re.compile(r'<\/%s>' % tag_name)
-            tag_closer = tag_closer_re.search(src, match.end())
-            if tag_closer:
-                msg = src[match.end():tag_closer.start()]
-                msg_out = ' gettext(%r) ' % msg
-                msg_length += len(msg_out)
-                out.write(msg_out)
-        delta = len(tag_str) - msg_length
-        if delta > 0:
-            out.write(''.join(repeat('X', delta)))
-        offset += len(tag_str)
-    return out.getvalue()
+class AngularBuildFile(makemessages.BuildFile):
+    def __init__(self, command, domain, translatable):
+        super().__init__(command, domain, translatable)
 
+    @cached_property
+    def is_templatized(self):
+        if self.domain is not 'djangular':
+            return super().is_templatized
+        return True
 
-@total_ordering
-class DjangularTranslatableFile(makemessages.TranslatableFile):
-
-    def process(self, command, domain):
+    @cached_property
+    def work_path(self):
         """
-        Override django TranslatableFile behavior for adding djangular domain
+        Path to a file which is being fed into GNU gettext pipeline. This may
+        be either a translatable or its preprocessed version.
         """
-        file_ext = os.path.splitext(self.file)[1]
-        if domain == 'djangular' and file_ext != '.py':
-            orig_file = os.path.join(self.dirpath, self.file)
-            work_file = orig_file
-            with io.open(orig_file, encoding=settings.FILE_CHARSET) as fp:
-                src_data = fp.read()
-            content = djangularize(src_data, orig_file[2:])
-            work_file = os.path.join(self.dirpath, '%s.py' % self.file)
-            with io.open(work_file, "w", encoding='utf-8') as fp:
-                fp.write(unicode(content))
+        if self.domain is not 'djangular':
+            return super().work_path
+
+        if not self.is_templatized:
+            return self.path
+
+        filename = '%s.%s' % (self.translatable.file, 'py')
+        return os.path.join(self.translatable.dirpath, filename)
+
+    def custom_preprocess(self):
+        """
+        Preprocess (if necessary) a translatable file before passing it to
+        xgettext GNU gettext utility.
+        """
+        with io.open(self.path, 'r', encoding=settings.FILE_CHARSET) as fp:
+            src_data = fp.read()
+
+        if self.domain == 'djangular':
+            content = self.process(src_data)
+
+        with io.open(self.work_path, 'w', encoding='utf-8') as fp:
+            fp.write(content)
+
+    def preprocess(self):
+        if self.domain == 'djangular':
+            self.custom_preprocess()
+        else:
+            super().preprocess()
+
+    def process(self, src_data):
+        if not src_data:
+            return src_data
+
+        regex = r"""{{\s*'(.*)'\s*\|\s*translate\s*}}"""
+        texts = re.compile(regex).findall(src_data)
+        return "\n".join(["_('{}')".format(text) for text in texts])
+
+
+class Command(makemessages.Command):
+    build_file_class = AngularBuildFile
+
+    def custom_process_locale_dir(self, locale_dir, files):
+        """
+        Extract translatable literals from the specified files, creating or
+        updating the POT file for a given locale directory.
+
+        Uses the xgettext GNU gettext utility.
+        """
+        build_files = []
+        for translatable in files:
+            if self.verbosity > 1:
+                self.stdout.write('processing file %s in %s\n' % (
+                    translatable.file, translatable.dirpath
+                ))
+            if self.domain != 'djangular':
+                continue
+            build_file = self.build_file_class(self, self.domain, translatable)
+            try:
+                build_file.preprocess()
+            except UnicodeDecodeError as e:
+                self.stdout.write(
+                    'UnicodeDecodeError: skipped file %s in %s (reason: %s)' % (
+                        translatable.file, translatable.dirpath, e,
+                    )
+                )
+                continue
+            build_files.append(build_file)
+
+        if self.domain == 'djangular':
+            # self.domain = 'django'
             args = [
                 'xgettext',
-                '-d', domain,
+                '-d', self.domain,
                 '--language=Python',
                 '--keyword=gettext_noop',
                 '--keyword=gettext_lazy',
@@ -87,63 +114,64 @@ class DjangularTranslatableFile(makemessages.TranslatableFile):
                 '--keyword=npgettext:1c,2,3',
                 '--keyword=pgettext_lazy:1c,2',
                 '--keyword=npgettext_lazy:1c,2,3',
-                '--output=-'
-            ] + command.xgettext_options
-            args.append(work_file)
-            msgs, errors, status = makemessages.gettext_popen_wrapper(args)
-            if errors:
-                if status != makemessages.STATUS_OK:
-                    os.unlink(work_file)
-                    raise CommandError(
-                        "errors happened while running xgettext on %s\n%s" %
-                        (self.file, errors))
-                elif command.verbosity > 0:
-                    # Print warnings
-                    command.stdout.write(errors)
-            if msgs:
-                # Write/append messages to pot file
-                if self.locale_dir is NO_LOCALE_DIR:
-                    file_path = os.path.normpath(os.path.join(self.dirpath,
-                                                              self.file))
-                    raise CommandError(
-                        "Unable to find a locale path to store"
-                        " translations for file %s" % file_path)
-                potfile = os.path.join(self.locale_dir, '%s.pot' % str(domain))
-                # Remove '.py' suffix
-                if os.name == 'nt':
-                    # Preserve '.\' prefix on Windows to respect gettext behavior
-                    old = '#: ' + work_file
-                    new = '#: ' + orig_file
-                else:
-                    old = '#: ' + work_file[2:]
-                    new = '#: ' + orig_file[2:]
-                msgs = msgs.replace(old, new)
-                makemessages.write_pot_file(potfile, msgs)
-            os.unlink(work_file)
+                '--output=-',
+            ]
         else:
-            super(DjangularTranslatableFile,
-                  self).process(command, domain)
+            return
 
+        input_files = [bf.work_path for bf in build_files]
+        with NamedTemporaryFile(mode='w+') as input_files_list:
+            input_files_list.write('\n'.join(input_files))
+            input_files_list.flush()
+            args.extend(['--files-from', input_files_list.name])
+            args.extend(self.xgettext_options)
+            msgs, errors, status = popen_wrapper(args)
 
-class Command(makemessages.Command):
+        if errors:
+            if status != STATUS_OK:
+                for build_file in build_files:
+                    build_file.cleanup()
+                raise CommandError(
+                    'errors happened while running xgettext on %s\n%s' %
+                    ('\n'.join(input_files), errors)
+                )
+            elif self.verbosity > 0:
+                # Print warnings
+                self.stdout.write(errors)
+
+        if msgs:
+            if locale_dir is NO_LOCALE_DIR:
+                file_path = os.path.normpath(build_files[0].path)
+                raise CommandError(
+                    'Unable to find a locale path to store translations for '
+                    'file %s' % file_path
+                )
+            for build_file in build_files:
+                msgs = build_file.postprocess_messages(msgs)
+            potfile = os.path.join(locale_dir, '%s.pot' % str(self.domain))
+            write_pot_file(potfile, msgs)
+
+        self.domain = 'djangular'
+
+        for build_file in build_files:
+            build_file.cleanup()
+
+    def process_locale_dir(self, locale_dir, files):
+        if self.is_djangular:
+            self.custom_process_locale_dir(locale_dir, files)
+        else:
+            super().process_locale_dir(locale_dir, files)
+
     def handle(self, *args, **kwargs):
         self.is_djangular = False
         if kwargs.get('domain') == 'djangular':
             self.is_djangular = True
             kwargs['domain'] = 'django'
+            kwargs['extensions'] = ['html']
         super(Command, self).handle(*args, **kwargs)
 
-    def find_files(self, root):
-        files = super(Command, self).find_files(root)
-        if self.is_djangular:
-            for idx, _file in enumerate(files):
-                if isinstance(_file, makemessages.TranslatableFile):
-                    files[idx] = DjangularTranslatableFile(_file.dirpath,
-                                                           _file.file,
-                                                           _file.locale_dir)
-        return files
-
     def build_potfiles(self):
+        # self.domain = 'django'
         if self.is_djangular:
             self.domain = 'djangular'
         return makemessages.Command.build_potfiles(self)
