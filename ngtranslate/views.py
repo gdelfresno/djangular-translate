8e4803ef4f2afd68afdfd8c090a5b48bbd09cb03
Guillermo del Fresno <gdelfresno@gmail.com>
2017-11-09 10:43:45 -0300

    Catalog view returns all catalogs.

--- ngtranslate/views.py
+++ ngtranslate/views.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-import json
-
 from django.http import JsonResponse
-from django.conf import settings
-from django.views.i18n import get_javascript_catalog
-from django.views.generic import View
 from django.utils.translation import (
-    check_for_language, get_language, to_locale,
-    get_language_from_request, activate
+    check_for_language, to_locale,
+    get_language_from_request
 )
+from django.views.i18n import get_javascript_catalog
 
 
 def djangular_catalog(request):
@@ -17,30 +12,7 @@ def djangular_catalog(request):
     if request.GET and 'lang' in request.GET:
         if check_for_language(request.GET['lang']):
             locale = to_locale(request.GET['lang'])
-    catalog, plural = get_javascript_catalog(locale, 'djangular',
-                                             ['django.conf'])
-    return JsonResponse(catalog)
-
-
-class LanguagesView(View):
-
-    def get(self, request):
-        result = {'languages': [], 'current': 0}
-        current = get_language_from_request(request)
-        idx = 0
-        for idx, item in enumerate(settings.LANGUAGES):
-            if item[0] == current:
-                result['current'] = idx
-            result['languages'].append({'code': item[0],
-                                        'name': unicode(item[1])})
-        return JsonResponse(result)
-
-    def post(self, request):
-        result = {'success': False}
-        request_data = json.loads(request.body)
-        language = request_data.get('lang')
-        if language:
-            activate(language)
-            request.session['django_language'] = get_language()
-            result = {'success': True}
-        return JsonResponse(result)
+    django_catalog, plural = get_javascript_catalog(locale, 'django', [])
+    angular_catalog, plural = get_javascript_catalog(locale, 'djangular', [])
+    django_catalog.update(angular_catalog)
+    return JsonResponse(django_catalog)
